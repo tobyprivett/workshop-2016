447d7279a5daa9db8bfa45f0cf28ba9ec0181c67
Toby Privett <toby@snaplab.co.uk>
2016-07-05 10:34:40 +0100

    Rendering a route

--- react-router/index.js
+++ react-router/index.js
@@ -1,4 +1,9 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { Router, Route, hashHistory } from 'react-router'
 import App from './modules/App'
-render(<App/>, document.getElementById('app'))
+
+render((
+  <Router history={hashHistory}>
+    <Route path="/" component={App} />
+  </Router>), document.getElementById('app'))
