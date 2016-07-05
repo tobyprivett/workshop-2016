359e203d55c2147fc61754ca36bdf69227558caf
Toby Privett <toby@snaplab.co.uk>
2016-07-05 10:44:07 +0100

    Add modules Repos and About

--- react-router/index.js
+++ react-router/index.js
@@ -2,8 +2,12 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Router, Route, hashHistory } from 'react-router'
 import App from './modules/App'
+import About from './modules/About'
+import Repos from './modules/Repos'
 
 render((
   <Router history={hashHistory}>
     <Route path="/" component={App} />
+    <Route path="/About" component={About} />
+    <Route path="/Repos" component={Repos} />
   </Router>), document.getElementById('app'))
