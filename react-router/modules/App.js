34fa5993cc7d1b8b40a6adc0bc15c2ce07767619
Toby Privett <toby@snaplab.co.uk>
2016-07-05 10:47:52 +0100

    Navigate with links

--- react-router/modules/App.js
+++ react-router/modules/App.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 export default React.createClass({
   render() {
-    return <div>Hello, Mundo!</div>
+    return (
+      <div>
+        <h1>Hello, Mundo!</h1>
+        <ul role='nav'>
+          <li><Link to='/About'>About</Link></li>
+          <li><Link to='/Repos'>Repos</Link></li>
+        </ul>
+      </div>
+    )
   }
 })
